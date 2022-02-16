6c71e2f10b2e30bd7f91d380891b2ffeb92feeca
Ndifreke Friday <ndiecodes@gmail.com>
2022-02-16 20:58:59 +0100

    chore: config

--- config.example.py
+++ config.example.py
@@ -1,5 +1,5 @@
 API_KEY = ""
 API_SECRET = ""
 TRADESYMBOL = "ETHUSDT"
-ASSETS = ["ETH", "USDT"]
+ASSETS = ["ETH", "USDT"] # Easier to get balance of either the traded and base asset with this eg. base asset - assets[1]
 TRADEQUANTITY = 2  # eg. buy or sell "2" eth
